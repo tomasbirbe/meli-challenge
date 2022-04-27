e1f88473857cf7c4ec6d64bbb6540cce5788b181
Tomas Birbe <tomas.birbe@gmail.com>
2022-04-27 11:52:06 -0300

    Main: Switch to only-desktop development

--- src/product/screens/Details.tsx
+++ src/product/screens/Details.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Box, Icon, Image, Stack, Text } from "@chakra-ui/react";
+import { BsStar, BsStarHalf, BsStarFill } from "react-icons/bs";
 
 import { Product } from "../types";
 import mock from "../mock";
@@ -7,8 +9,55 @@ interface Props {
   product: Product;
 }
 
-const DetailsScreen: React.FC<Props> = ({ product = mock.product }) => {
-  return <div>{`<DetailsScreen /> para ${product.title}`}</div>;
-};
-
-export default DetailsScreen;
+export default function DetailScreen({ product = mock.product }: Props) {
+  return (
+    <Box paddingBlock={4} paddingInline={4}>
+      <Stack spacing={0}>
+        <Text color="blackAlpha.600" fontSize={12} marginBlockEnd={2}>
+          Nuevo | 1024 vendidos
+        </Text>
+        <Text lineHeight={1.3} marginBlockEnd={1}>
+          {product.title}
+        </Text>
+        <Stack align="center" color="blue.500" direction="row" spacing={1}>
+          <Icon as={BsStarFill} boxSize={2.5} />
+          <Icon as={BsStarFill} boxSize={2.5} />
+          <Icon as={BsStarFill} boxSize={2.5} />
+          <Icon as={BsStarFill} boxSize={2.5} />
+          <Icon as={BsStarHalf} boxSize={2.5} />
+          <Text color="blackAlpha.600" fontSize={14} paddingInlineStart={1}>
+            53 opiniones
+          </Text>
+        </Stack>
+        <Stack align="center" direction="row" paddingBlockStart={2}>
+          <Text
+            bg="orange.500"
+            borderRadius={3}
+            color="white"
+            fontSize={10}
+            paddingInline={1}
+            width="fit-content"
+          >
+            MÁS VENDIDO
+          </Text>
+          <Text color="blue.500" fontSize={12}>
+            7° en peluches
+          </Text>
+        </Stack>
+        <Stack direction="row" height="256px" overflowX="auto" width="full">
+          {product.pictures?.map((picture) => {
+            return (
+              <Image
+                key={picture.id}
+                height="256px"
+                loading="lazy"
+                src={picture.url}
+                width="256px"
+              />
+            );
+          })}
+        </Stack>
+      </Stack>
+    </Box>
+  );
+}
